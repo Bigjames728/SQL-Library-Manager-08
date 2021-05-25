8a12bc085a2be610dfd41f20eb82c74d77abe19c
Bigjames728 <bigjames728@gmail.com>
2021-05-24 18:10:32 -0700

    Fixed syn and authentication in app.js

--- app.js
+++ app.js
@@ -23,16 +23,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-sequelize.sync().then(function(){
-  server.listen(port);
-});
-
 (async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
+  }
+
+  try {
+    await sequelize.sync();
+    console.log('database synced');
+  } catch (error) {
+    console.log('Unable to sync the database', error);
   }
 })();
 

080d88c49378e2d0d825bd1b94db4c8ed79cc1a5
Bigjames728 <bigjames728@gmail.com>
2021-05-25 15:44:41 -0700

    removed filler code and tried fixing error handler

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -43,20 +43,16 @@ app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   console.log('404 error handler called');
-  const err = new Error("It looks like this page doesn't exist.");
+  const err = new Error("It looks like this page doesn't exists.");
   res.status(404);
-  res.render('page-not-found', {error: err});
+  res.render('page-not-found', {err});
+  
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  
 });
+
 
 module.exports = app;

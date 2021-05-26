90b7b78c958b74ad45d182bdb2552b028772db1a
Bigjames728 <bigjames728@gmail.com>
2021-05-25 20:06:17 -0700

    Trying to get home route to work

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,22 @@ var express = require('express');
 var router = express.Router();
 var Book = require('../models').Book;
 
+
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
 /* GET home page. */
-router.get('/', async (req, res, next) => {
+router.get('/books', asyncHandler (async(req, res, next) => {
   // res.render('index', { title: 'Express' });
   const books = await Book.findAll();
-  res.json({ books: books });
-});
+  res.render('index', { books: books });
+}));
 
 module.exports = router;
